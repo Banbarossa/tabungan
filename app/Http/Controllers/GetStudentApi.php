6432e2c75efa5c1bbefac76627107d1889fc05ce
Banbarossa <banbarossa@gmail.com>
2025-05-27 14:41:52 +0700

    update

--- app/Http/Controllers/GetStudentApi.php
+++ app/Http/Controllers/GetStudentApi.php
@@ -49,7 +49,7 @@ class GetStudentApi extends Controller
             foreach($data as $hp){
                 $student = Student::where('absen_id',$hp['absen_student_id'])->first();
                 if($student){
-                    $student->update(['notification_account',$hp['no_hp']]);
+                    $student->update(['notification_account'=>$hp['no_hp']]);
                 }
             }
 
