7c94bf7a309d394a89be5676b1f6c6aae765136c
Banbarossa <banbarossa@gmail.com>
2025-05-28 00:08:29 +0700

    update

--- resources/views/livewire/admin/dashboard/master-dasboard.blade.php
+++ resources/views/livewire/admin/dashboard/master-dasboard.blade.php
@@ -5,7 +5,7 @@
         <livewire:admin.dashboard.total-saldo/>
         <livewire:admin.dashboard.total-withdraw-today/>
 
-        <div class="relative aspect-video overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
+        <div class="hidden md:block relative aspect-video overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
             <x-placeholder-pattern class="absolute inset-0 size-full stroke-gray-900/20 dark:stroke-neutral-100/20" />
         </div>
     </div>
