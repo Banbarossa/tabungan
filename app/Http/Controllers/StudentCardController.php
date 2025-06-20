4ef1d0d7ff5a0b17e80d898ff810fd5a98fbfe71
Banbarossa <banbarossa@gmail.com>
2025-06-20 18:17:45 +0700

    cetak kartu yang ada nisn saja

--- app/Http/Controllers/StudentCardController.php
+++ app/Http/Controllers/StudentCardController.php
@@ -20,7 +20,7 @@ class StudentCardController extends Controller
 
 
         if($id){
-            $siswas = Student::where('id',$id)->whereNotNull('nisn')->get()->map(function($siswa)use ($dns1d, $dns2d)  {
+            $siswas = Student::where('id',$id)->get()->map(function($siswa)use ($dns1d, $dns2d)  {
 
             $siswa->qr = 'data:image/png;base64,' . $dns2d->getBarcodePNG($siswa->nisn, 'QRCODE',5,5);
 
@@ -33,7 +33,7 @@ class StudentCardController extends Controller
 
             });
         }else{
-            $siswas = Student::whereStatus(true)->orderBy('name')->get()->map(function($siswa) use ($dns1d, $dns2d) {
+            $siswas = Student::whereStatus(true)->whereNotNull('nisn')->orderBy('name')->get()->map(function($siswa) use ($dns1d, $dns2d) {
                 $nisn=$siswa->nisn;
                 $siswa->qr = 'data:image/png;base64,' . $dns2d->getBarcodePNG($nisn, 'QRCODE',5,5);
                 $siswa->barcode = 'data:image/png;base64,' . $dns1d->getBarcodePNG($nisn, 'C128',1,50,[0,0,0]);
