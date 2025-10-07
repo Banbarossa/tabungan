36649c05cba3be6e102ac9b0a89a03b3d6653eb0
Banbarossa <banbarossa@gmail.com>
2025-10-07 08:02:38 +0700

    test schedule

--- routes/console.php
+++ routes/console.php
@@ -3,8 +3,15 @@
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote');
+//Artisan::command('inspire', function () {
+//    $this->comment(Inspiring::quote());
+//})->purpose('Display an inspiring quote');
 
-Schedule::command('backup:run')->dailyAt('00:00');
+//Schedule::command('backup:run')->everyFiveMinutes();
+//Schedule::call(function (){
+//    Log::info('schedul run at' .now());
+//})->everyMinute();
+
+Schedule::call(function () {
+    Log::info('schedule run at ' . now());
+})->everyMinute();
