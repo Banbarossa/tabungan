3cfd6f36d2f525f7aee29a1977991ecf821389c6
Banbarossa <banbarossa@gmail.com>
2026-08-18 17:58:56 +0700

    update

--- resources/views/components/layouts/app/sidebar.blade.php
+++ resources/views/components/layouts/app/sidebar.blade.php
@@ -1,8 +1,21 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="light">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="light" style="color-scheme: light;">
 
 <head>
     @include('partials.head')
+    <style>
+        html {
+            color-scheme: light !important;
+        }
+
+        body {
+            color-scheme: light !important;
+        }
+
+        .dark {
+            color-scheme: light !important;
+        }
+    </style>
 </head>
 
 <body class="min-h-screen bg-slate-100 dark:bg-zinc-800">
